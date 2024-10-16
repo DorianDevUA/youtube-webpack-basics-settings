98d1588cf741339e1947039e1d1726c3b593460e
Dorian Devua <Dorian.DevUa@gmail.com>
2024-10-16 21:14:05 +0400

    add image-webpack-loader

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import './index.html';
 import './index.scss';
 import { mult, sum } from './modules/calc';
+import logo from './images/logocorel_new_23.01.21_mini.png';
+
+const refs = {
+  imageContainer: document.querySelector('.js-imageContainer'),
+};
+
+const image = new Image();
+image.src = logo;
+image.width = 100;
+image.height = 100;
+refs.imageContainer.append(image);
 
 console.log(mult(2, 8));
 console.log(sum(3, 4));
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const PostcssPresetEnv = require('postcss-preset-env');
-const { type } = require('os');
 
 const mode = process.env.NODE_ENV || 'development';
 const devMode = mode === 'development';
@@ -19,17 +17,21 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
     filename: '[name].[contenthash].js',
+    assetModuleFilename: 'assets/[name][ext]',
   },
   module: {
     rules: [
+      // Робота з html файлами
       {
         test: /\.html$/i,
         loader: 'html-loader',
       },
+      // Робота зі стилями. Файли sass, scss, css
       {
         test: /\.(c|sa|sc)ss$/i,
         use: [devMode ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
       },
+      // Робота зі шрифтами. Файли ttf, eot, woff, woff2
       {
         test: /\.(woff2?|eot|ttf|otf)$/i,
         type: 'asset/resource',
@@ -37,6 +39,40 @@ module.exports = {
           filename: 'fonts/[name][ext]',
         },
       },
+      // Робота з зображеннями. Файли jpeg, jpg, png, webp, gif, svg
+      {
+        test: /\.(jpe?g|png|webp|gif|svg)$/i,
+        use: [
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.9],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75,
+              },
+            },
+          },
+        ],
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]',
+        },
+      },
+      // Робота з файлами JavaScript. Файли js, mjs, cjs
       {
         test: /\.(?:js|mjs|cjs)$/i,
         exclude: /node_modules/,
